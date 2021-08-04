e96e70d86a6c8da0a90361944b9ee9064c8b72de
Diego Gonzalez <diego.ags04@gmail.com>
2021-08-04 07:48:00 -0400

    fixed animations bug when open and closing modals

--- hooks/use-modal.tsx
+++ hooks/use-modal.tsx
@@ -51,11 +51,11 @@ export const useModal = () => {
 						<Transition.Child
 							as="div"
 							enter="ease-out duration-300"
-							enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-							enterTo="opacity-100 translate-y-0 sm:scale-100"
+							enterFrom="opacity-0"
+							enterTo="opacity-100"
 							leave="ease-in duration-200"
-							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+							leaveFrom="opacity-100"
+							leaveTo="opacity-0"
 							className="z-20"
 						>
 							<div ref={cancelButtonRef}>{children}</div>
